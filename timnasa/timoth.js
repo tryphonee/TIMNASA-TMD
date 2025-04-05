cf7dc6e5c4b5e2e5a41c93925234e122741a3f2f
MTALEE <tryphonee3@gmail.com>
2025-04-05 15:44:07 +0300

    Add files via upload

---
+++ timnasa/timoth.js
@@ -0,0 +1,17 @@
+
+var tabCmds = [];
+let cm = [];
+function timomth(obj, fonctions) {
+    let infoComs = obj;
+    if (!obj.categorie) {
+        infoComs.categorie = "General";
+    }
+    if (!obj.reaction) {
+        infoComs.reaction = "🙂";
+    }
+    infoComs.fonction = fonctions;
+    cm.push(infoComs);
+    // console.log('chargement...')
+    return infoComs;
+}
+module.exports = { timoth, Module: timoth, cm };
